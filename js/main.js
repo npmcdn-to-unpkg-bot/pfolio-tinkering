a127db2281ed1ee7e7fdc4fc6285af32bab9e5af
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-08-26 13:22:52 -0700

    slideshow function updated

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-function slideSwitch(){
-    var $active = $(#slideshow img.active);
+function slideSwitch() {
+    var $active = $('#slideshow img.active');
     var $next = $active.next();
 
-    $next.addClass('active');
-    $active.removeClass('active');
+    $active.addClass('last-active');
 
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function(){
+            $active.removeClass('active last-active');
+        });
 }
 
-$(function(){
-    setInterval('slideSwitch()', 5000);
+$(function() {
+    setInterval( "slideSwitch()", 5000 );
 });
