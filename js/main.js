cf48cc2f3b8acccc329b3cf8a645ffded5e9d0dd
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-08-29 13:07:25 -0700

    adding comments to code, adding button functionality

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
+$('#menu-button').click(function(){
+    $('#menu').toggle();
+});
+
+// Slideshow functions
 function slideSwitch() {
-    var $active = $('#slideshow img.active');
+    var $active = $('#slideshow img.active'); // Sets active variable based on class
 
-    if ( $active.length == 0 ) $active = $('#slideshow img:last');
+    if ( $active.length == 0 ) $active = $('#slideshow img:last'); // Sets default image to active
 
-    var $next =  $active.next().length ? $active.next()
-        : $('#slideshow img:first');
+    var $next =  $active.next().length ? $active.next() // If active.next DOM element is present, sets variable to next
+        : $('#slideshow img:first');                    // If not, next loops back to top of list in DOM
 
-    $active.addClass('last-active');
+    $active.addClass('last-active');        // Add class to active bump it down on z-index by one level
 
-    $next.css({opacity: 0.0})
-        .addClass('active')
-        .animate({opacity: 1.0}, 1000, function() {
-            $active.removeClass('active last-active');
+    $next.css({opacity: 0.0})              // Set opacity of next image to zero
+        .addClass('active')                // Add class to bump up to visible level
+        .animate({opacity: 1.0}, 1000, function() {    // Animate opacity to 100
+            $active.removeClass('active last-active'); // Remove classes from previously active image
         });
 }
 
 $(function() {
-    setInterval( "slideSwitch()", 5000 );
+    setInterval( "slideSwitch()", 5000 );  // Call function at 5-second intervals
 });
